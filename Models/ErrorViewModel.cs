188618e545be8fd1a94f471f9344c580a1b572ec
ShCodder <nomail@nomail.com>
2020-11-12 00:20:16 +0300

    asp net core 3.1 mvc - create project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace webEducation.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
